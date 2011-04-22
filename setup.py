4e83ae29671d124fdf6fffeb11eb869c9f088ed2
lcr <none@none>
2011-04-22 15:08:22 -0600

    - fix setup.py
    --HG--
    branch : pu

--- setup.py
+++ setup.py
@@ -1,54 +1,30 @@
-# Copyright (c) 2011 L. C. Rees.  All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
-#
-# 1.  Redistributions of source code must retain the above copyright notice,
-# this list of conditions and the following disclaimer.
-# 2.  Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-# 3.  Neither the name of the tubing project nor the names
-# of its contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-
 '''setup - setuptools based setup for tubing'''
 
-#try:
-#    from setuptools import setup
-#except:
-#    from distutils.core import setup
-#
-#setup(
-#    name='appspace',
-#    version='0.1',
-#    description='''Loose coupled Python application plumbing''',
-#    long_description='''Loose coupled Python application plumbing''',
-#    author='L. C. Rees',
-#    author_email='lcrees@gmail.com',
-#    license='ZPL',
-#    packages = ['appspace'],
-#    zip_safe = False,
-#    keywords='application composition component injection aspect-oriented spring',
-#    classifiers=[
-#        'Development Status :: 3 - Alpha',
-#        'Intended Audience :: Developers',
-#        'License :: OSI Approved :: ZPL License',
-#        'Natural Language :: English',
-#        'Operating System :: OS Independent',
-#        'Programming Language :: Python',
-#    ],#
-#    install_requires = ['zope.component>=3.10.0']
-#)
+try:
+    from setuptools import setup
+except:
+    from distutils.core import setup
+
+setup(
+    name='appspace',
+    version='0.1',
+    description='''Loose coupled Python application plumbing''',
+    long_description='''Loose coupled Python application plumbing''',
+    author='L. C. Rees',
+    author_email='lcrees@gmail.com',
+    license='ZPL',
+    packages = ['appspace'],
+    packages = ['appspace', 'appspace.test'],
+    test_suite='appspace.test',
+    zip_safe = False,
+    keywords='application composition component injection aspect-oriented spring',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ZPL License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = ['zope.component>=3.10.0']
+)

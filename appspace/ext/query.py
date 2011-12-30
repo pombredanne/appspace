12ff2cbc60dd88050b0f5202dc98e841cc42afef
lcr <kwarterthieves@yahoo.com>
2011-12-30 09:26:30 -0700

    - query work

--- appspace/ext/query.py
+++ appspace/ext/query.py
@@ -37,8 +37,11 @@ class Query(deque):
                 self._this = kw.pop('this', None)
             else:
                 raise NoAppError('appspace not found')
+        # manager
         self.manager = self._appspace.manager
+        # settings
         self.settings = self._appspace.manager.settings
+        # enable for traits
         self._enable = True
         deque.__init__(self, *args)
 
@@ -51,6 +54,7 @@ class Query(deque):
 
         @param this: the thing
         '''
+        # clear queue
         self.clear()
         # append to queue
         self.append(this)
@@ -64,18 +68,24 @@ class Query(deque):
         @param branch: branch label (default: False)
         @param app: new application (default: False)
         '''
+        # process new application
         if app:
+            # use branch manager
             if branch:
                 manager = self.branch(branch).one().manager
+            # use passed manager
             else:
                 manager = self.manager
+            # add to appspace
             manager.set(label, app)
             return self._tail(app)
+        # return app from branch
         if branch:
             return self._tail(self._appspace[branch][label])
+        # return from primary appsapce
         return self._tail(self._appspace[label])
 
-    def apply(self, label, branch='', *args, **kw):
+    def apply(self, label, branch=False, *args, **kw):
         '''
         call application from appspace
 
@@ -90,12 +100,14 @@ class Query(deque):
         build new appspace
 
         @param pattern: pattern configuration class or appspace label
-        @param required: required settings
-        @param defaults: default settings
+        @param required: required settings (default: None)
+        @param defaults: default settings (default: None)
         @param *args: tuple of module paths or component inclusions
         '''
+        # from appspace configuration class...
         if issubclass(pattern, Patterns):
             return cls(pattern.build(required, defaults))
+        # from label and arguments...
         elif isinstance(pattern, basestring) and args:
             return cls(Patterns.settings(
                 patterns(pattern, *args, **kw), required, defaults,
@@ -107,21 +119,22 @@ class Query(deque):
         bind app to event
 
         @param event: event label
-        @param label: label of app
+        @param label: application label
         @param branch: branch label (default: False)
         '''
-        app = self.app(label, branch)
-        self.manager.events.bind(event, app)
+        self.manager.events.bind(event, self.app(label, branch))
         return self
 
     def branch(self, label):
         '''
-        add or get branch appspace
+        add or fetch branch appspace
 
         @param label: label of appspace
         '''
+        # fetch branch if exists...
         try:
             return self._tail(self._appspace[label])
+        # create new branch
         except NoAppError:
             new_appspace = Appspace(Manager())
             self.manager.set(label, new_appspace)
@@ -130,65 +143,51 @@ class Query(deque):
 
     def burst(self, label, queue):
         '''
-        run event subscribers on contents of queue
+        process event subscribers on contents of queue
 
         @param label: event label
         @param queue: queued arguments
         '''
         return self._tail(self.manager.events.burst(label, queue))
 
-    def event(self, label, priority=None, **kw):
+    def event(self, label, priority=False, **kw):
         '''
         create new event
 
         @param event: event label
-        @param priority: priority of event
+        @param priority: priority of event (default: False)
         '''
-        if priority is None or not kw:
+        # unregister event
+        if not priority or not kw:
             self.manager.unregister(label)
             return self
-        event = self.manager.register(label, priority, **kw)
-        return self._tail((label, event))
+        # register event if priority and keywords passed
+        return self._tail(self.manager.register(label, priority, **kw))
 
     def enable(self):
         '''toggle if trait events are allowed'''
-        self._enable = not self._enable
-        return self._tail(self._enable)
+        return self._tail(setter(self, '_enable', not self._enable))
 
-    def find(self, iterable, label, branch=False):
+    def find(self, data, label, branch=False):
         '''
-        find first item in iterable for which app in appspace is true
+        get first item in data for which app in appspace is true
 
-        @param iterable: iterable to process
+        @param data: data to process
         @param label: application label
         @param branch: branch label (default: False)
         '''
-        for item in ifilter(self.app(label, branch).one(), iterable):
+        for item in ifilter(self.app(label, branch).one(), data):
             return self._tail(item)
 
-    def filter(self, iterable, label, branch=False):
+    def filter(self, data, label, branch=False):
         '''
-        fetch items from iterable for which app in appspace is true
+        get items from data for which app in appspace is true
 
-        @param iterable: iterable to process
+        @param data: data to process
         @param label: application label
         @param branch: branch label (default: False)
         '''
-        return self(
-            i for i in ifilter(self.app(label, branch).one(), iterable)
-        )
-
-    def groupby(self, iterable, label, branch=False):
-        '''
-        group items from iterable by criteria of app in appspace
-
-        @param iterable: iterable to process
-        @param label: application label
-        @param branch: branch label (default: False)
-        '''
-        return self(
-            i for i in groupby(iterable, self.app(label, branch).one())
-        )
+        return self(i for i in ifilter(self.app(label, branch).one(), data))
 
     def fire(self, event, *args, **kw):
         '''
@@ -197,6 +196,16 @@ class Query(deque):
         @param event: event label
         '''
         return self._tail(self.manager.events.fire(event, *args, **kw))
+
+    def groupby(self, data, label, branch=False):
+        '''
+        group items from data by criteria of app in appspace
+
+        @param data: data to process
+        @param label: application label
+        @param branch: branch label (default: False)
+        '''
+        return self(i for i in groupby(data, self.app(label, branch).one()))
 
     def last(self):
         '''fetch one last result'''
@@ -229,22 +238,21 @@ class Query(deque):
             '_'.join([modname(self._this), clsname(self._this)]).lower()
         )
 
-    def map(self, iterable, label, branch=False):
+    def map(self, data, label, branch=False):
         '''
-        apply app in appspace to each item in iterable
+        apply app in appspace to each item in data
 
-        @param iterable: iterable to process
+        @param data: data to process
         @param label: application label
         @param branch: branch label (default: False)
         '''
-        app = self.app(label, branch).one()
-        return self(i for i in imap(app, iterable))
+        return self(i for i in imap(self.app(label, branch).one(), data))
 
     def members(self, tester=False):
         '''
         fetch object members by their class
 
-        @param tester: test to filter by
+        @param tester: test to filter by (default: False)
         '''
         if tester:
             if isclass(tester):
@@ -253,7 +261,9 @@ class Query(deque):
                 test = tester
         else:
             test = ismethod
-        return self(ifilter(test, (i for i in itermembers(self._this))))
+        return self(
+            i for i in ifilter(test, (i for i in itermembers(self._this)))
+        )
 
     def one(self):
         '''fetch one result'''
@@ -264,17 +274,16 @@ class Query(deque):
 
     first = one
 
-    def reduce(self, iterable, label, branch=False, initial=None):
+    def reduce(self, data, label, branch=False, initial=None):
         '''
-        reduce iterable to single value with app in appspace
+        reduce data to single value with app in appspace
 
-        @param iterable: iterable to process
+        @param data: data to process
         @param label: application label
         @param branch: branch label (default: False)
+        @param initial: initial value (default: None)
         '''
-        return self._tail(
-            reduce(self.app(label, branch).one(), iterable, initial)
-        )
+        return self._tail(reduce(self.app(label, branch).one(), data, initial))
 
     def register(self, model):
         '''
@@ -288,29 +297,30 @@ class Query(deque):
         setter(model, 'S', self.settings)
         return self._tail(model)
     
-    def reject(self, iterable, label, branch=False):
+    def reject(self, data, label, branch=False):
         '''
-        fetch items from iterable for which app in appspace is false
+        fetch items from data for which app in appspace is false
 
-        @param iterable: iterable to process
+        @param data: data to process
         @param label: application label
         @param branch: branch label (default: False)
         '''
         return self(
-            i for i in ifilterfalse(self.app(label, branch).one(), iterable)
+            i for i in ifilterfalse(self.app(label, branch).one(), data)
         )
     
-    def right_reduce(self, iterable, label, branch=False, initial=None):
+    def right_reduce(self, data, label, branch=False, initial=None):
         '''
-        reduce iterable to single value with app in appspace from right side
+        reduce data to single value with app in appspace from right side
 
-        @param iterable: iterable to process
+        @param data: data to process
         @param label: application label
         @param branch: branch label (default: False)
+        @param initial: initial value (default: None)
         '''
         return self._tail(reduce(
             lambda x, y: self.app(label, branch).one()(y, x), 
-            reversed(iterable),
+            reversed(data),
             initial,
         ))
 
@@ -318,15 +328,14 @@ class Query(deque):
         '''
         change setting in application settings
 
-        @param key: name of settings
-        @param value: value in settings
+        @param key: setting key
+        @param value: value in settings (default: NoDefaultSpecified)
         @param default: setting value (default: None)
         '''
         if value is not NoDefaultSpecified:
             self.settings.set(key, value)
             return self
         return self._tail(self.settings.get(key, default))
-
 
     def trigger(self, label):
         '''
@@ -341,7 +350,7 @@ class Query(deque):
         unbind app from event
 
         @param event: event label
-        @param label: label of app
+        @param label: application label
         @param branch: branch label (default: False)
         '''
         self.manager.events.unbind(event, self.app(label, branch).one())

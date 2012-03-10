572373002d9c64edd2d2bd6b20890a6423540632
lcr <kwarterthieves@yahoo.com>
2012-03-10 01:55:29 -0700

    - update change log
    --HG--
    branch : pu

--- appspace/__init__.py
+++ appspace/__init__.py
@@ -10,7 +10,7 @@ from appspace.keys import NoAppError, AppLookupError
 from appspace.builders import patterns, class_patterns
 from appspace.spaces import Branch, Namespace, Patterns, include
 
-__version__ = (0, 5, 0)
+__version__ = (0, 5, 1)
 __all__ = sorted(name for name, obj in items(locals()) if not any([
     name.startswith('_'), ismodule(obj),
 ]))
